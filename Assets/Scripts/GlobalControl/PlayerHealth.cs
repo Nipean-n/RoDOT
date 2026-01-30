4714a5cd43f838f937f2868dca469553d8804293
wldxiaobai <944418001@qq.com>
2026-01-31 04:27:17 +0800

    add player and healthbar

---
+++ Assets/Scripts/GlobalControl/PlayerHealth.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Globalizer<PlayerHealth>
+{
+    [Tooltip("控制血条UI的脚本对象")]
+    [SerializeField] private HealthBarControl healthBarUI;
+    [Tooltip("控制血条数字的脚本对象")]
+    [SerializeField] private HealthBarNumber healthBarNum;
+    [Tooltip("最大生命值")]
+    [SerializeField] private int maxHealth = 100;
+    [Tooltip("是否开启调试功能")]
+    [SerializeField] bool debugMode = false;
+
+    private int currentHealth;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        currentHealth = maxHealth;
+        healthBarUI.UpdateHealthBar((float)currentHealth / maxHealth);
+        healthBarNum.UpdateHealthNumber(currentHealth, maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
+        healthBarUI.UpdateHealthBar((float)currentHealth / maxHealth);
+        healthBarNum.UpdateHealthNumber(currentHealth, maxHealth);
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Min(currentHealth, maxHealth);
+        healthBarUI.UpdateHealthBar((float)currentHealth / maxHealth);
+        healthBarNum.UpdateHealthNumber(currentHealth, maxHealth);
+    }
+
+    private void Update()
+    {
+        if (debugMode)
+        {
+            if (Input.GetKeyDown(KeyCode.Equals))
+            {
+                Heal(10);
+            }
+            if (Input.GetKeyDown(KeyCode.Minus))
+            {
+                TakeDamage(10);
+            }
+        }
+
+    }
+}
