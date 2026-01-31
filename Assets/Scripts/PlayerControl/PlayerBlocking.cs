441585b3eb80cebb087b49ed50832800cda5b474
Nipean-n <ztytzty@163.com>
2026-01-31 15:15:38 +0800

    添加了轻攻击和重攻击预制体和标签，添加了playerblocking脚本

---
+++ Assets/Scripts/PlayerControl/PlayerBlocking.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlocking : MonoBehaviour
+{
+   
+    void Update()
+    {
+        GameObject[] allAttacks1 = GameObject.FindGameObjectsWithTag("Attack1");
+        GameObject[] allAttacks2 = GameObject.FindGameObjectsWithTag("Attack2");
+    }
+}
